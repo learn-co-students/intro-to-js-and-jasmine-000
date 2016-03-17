d273ce0b2baee1c69256a39c05824708797b3f1a
NoneOfMaster <rcraigt@gmail.com>
2016-03-17 12:38:44 -0700

    seems like there is a lab needed here on browser and JS workflow

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 'use strict';
+function favIceCream(fav_flav){
+    return "I love " + fav_flav
+}
+
+function shouting(shout){
+    return shout.toUpperCase()
+}
+
+function roundDown(number){
+    return Math.floor(number)
+}
+
+function theTruth(){
+    return true
+}
